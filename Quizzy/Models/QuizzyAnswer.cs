3cbf6a5ea21a4cf8bf26cb3a0d17fcaf96e0c8a6
Lyuben Dimitrov <ldimitrov070@gmail.com>
2015-01-25 18:52:34 +0100

    fixed typo

--- Quizzy/Models/QuizzyAnswer.cs
+++ Quizzy/Models/QuizzyAnswer.cs
@@ -1,11 +1,4 @@
-﻿// ----------------------------------------------------------------------------------
-// Microsoft Developer & Platform Evangelism
-// 
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// 
-// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
-// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
-/*
+﻿/*
  * Represents the option selected by the user in response to a quiz question
  */
 namespace Quizzy.Models
@@ -26,6 +19,6 @@ namespace Quizzy.Models
         public int QuestionId { get; set; }
 
         [JsonIgnore]
-        public virtual QuizzyOption TriviaOption { get; set; }
+        public virtual QuizzyOption QuizzyOption { get; set; }
     }
 }
